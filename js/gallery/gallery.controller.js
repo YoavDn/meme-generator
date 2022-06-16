19e47c137efaece9bd2e312d0fc3f1e383d7f9c3
yoav <yoavdan.m@gmail.com>
2022-06-16 09:44:49 +0300

    deplow to netlify

--- js/gallery/gallery.controller.js
+++ js/gallery/gallery.controller.js
@@ -1,8 +1,12 @@
 'use strict'
 
+renderCards()
+
 function onImgSelect(e) {
   const elGallerySection = document.querySelector('.gallery-section')
   const elEditorSection = document.querySelector('.editor-section')
+
+  if (e.target.parentElement.classList[0] !== 'card') return
   const imgId = e.target.parentElement.dataset.imgId
 
   elGallerySection.classList.add('hidden')
@@ -10,6 +14,17 @@ function onImgSelect(e) {
 
   emptyElsFromMeme()
   setImg(imgId)
+}
+
+function renderCards() {
+  const elCardsContainer = document.querySelector('.gallery-container')
+  const imgs = getImgs()
+
+  elCardsContainer.innerHTML = imgs
+    .map((img, idx) => {
+      return `<div class="card shadow" data-img-id="${idx + 1}"><img src="${img.url}" alt="" /></div>`
+    })
+    .join('')
 }
 
 function onShowGallery() {
